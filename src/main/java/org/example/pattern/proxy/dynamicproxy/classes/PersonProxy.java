7c56d7523af644c72085e0e1e541e5659fb9f073
jy lim <imjee02@gmail.com>
2022-11-01 16:51:42 +0900

    예제 소스 추가

--- src/main/java/org/example/pattern/proxy/dynamicproxy/classes/PersonProxy.java
+++ src/main/java/org/example/pattern/proxy/dynamicproxy/classes/PersonProxy.java
@@ -1,6 +1,7 @@
 package org.example.pattern.proxy.dynamicproxy.classes;
 
 import org.example.pattern.proxy.dynamicproxy.interfaces.Person;
+import org.example.pattern.proxy.example.PersonBean;
 
 import java.lang.reflect.Proxy;
 
@@ -8,12 +9,18 @@ public class PersonProxy {
     
     
     //Proxy객체를 만들어 proxy를 리턴함
-    
+
     public Person getOwnerProxy(Person person) {
+
         return (Person) Proxy.newProxyInstance(
                 person.getClass().getClassLoader(),
-                person.getClass().getInterfaces() , //이 클래스가 상속받을 인터페이스
-                new OwnerInvocationHandler(person)); // 이 클래스가 만들 객체 => 얘기 invoke를 실행함
+                person.getClass().getInterfaces(),
+                new OwnerInvocationHandler(person));
+//
+//        return (Person) Proxy.newProxyInstance(
+//                person.getClass().getClassLoader(),
+//                person.getClass().getInterfaces() , //이 클래스가 상속받을 인터페이스
+//                new OwnerInvocationHandler(person)); // 이 클래스가 만들 객체 => 얘기 invoke를 실행함
     }
 
     public Person getNonOwnerProxy(Person person) {
--- src/main/java/org/example/pattern/proxy/dynamicproxy/run/ProxyTest.java
+++ src/main/java/org/example/pattern/proxy/dynamicproxy/run/ProxyTest.java
@@ -1,21 +1,40 @@
 package org.example.pattern.proxy.dynamicproxy.run;
 
+import org.example.pattern.proxy.dynamicproxy.classes.NonOwnerInvocationHandler;
+import org.example.pattern.proxy.dynamicproxy.classes.OwnerInvocationHandler;
 import org.example.pattern.proxy.dynamicproxy.classes.PersonImpl;
 import org.example.pattern.proxy.dynamicproxy.classes.PersonProxy;
 import org.example.pattern.proxy.dynamicproxy.interfaces.Person;
+
+import java.lang.reflect.Proxy;
 
 public class ProxyTest {
 
     public static void main(String[] args) {
         
-        Person jeeyoung = new PersonImpl("지영");
+        Person jeeyoung = new PersonImpl("지영쿤");
         PersonProxy personProxy = new PersonProxy();
         Person ownerProxy = personProxy.getOwnerProxy(jeeyoung);
 
-        ownerProxy.setGender("여자");
         ownerProxy.setInterests("멍때리기");
 
         System.out.println(ownerProxy);
 
     }
+
+    public static Person getOwnerProxy(Person person) {
+        return (Person) Proxy.newProxyInstance(
+                Thread.currentThread().getContextClassLoader(),
+                // person.getClass().getInterfaces() , //이 클래스가 상속받을 인터페이스
+                new Class<?>[]{Person.class} , //이 클래스가 상속받을 인터페이스
+                new OwnerInvocationHandler(person)); // 이 클래스가 만들 객체 => 얘기 invoke를 실행함
+    }
+
+    public Person getNonOwnerProxy(Person person) {
+        return (Person) Proxy.newProxyInstance(
+                person.getClass().getClassLoader(),
+                person.getClass().getInterfaces() ,
+                new NonOwnerInvocationHandler(person));
+    }
+
 }
